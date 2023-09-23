ba77116841a76f8b806669b52d36040731cafdeb
kamiltarach <113509888+kamiltarach@users.noreply.github.com>
2023-09-23 10:31:01 +0200

    JS changes
    Now the js code has been slightly improved
    The css code has also been changed

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
-// Funkcja do sprawdzania, czy tekst zawija się na sekcję "About"
-function checkTextWrap() {
-  const hero = document.querySelector('#hero');
-  const about = document.querySelector('#about');
-  const parallax = document.querySelector('#hero');
-  const textToHide = document.querySelector('.hero-text'); // Załóżmy, że element z tekstem ma klasę 'hero-text'
+// Zmienne do przechowywania elementów sekcji
+const aboutSection = document.querySelector('#about');
+const tipsSection = document.querySelector('#tips');
+const projectsSection = document.querySelector('#projects');
+const contactSection = document.querySelector('#contact');
 
-  if (!hero || !about || !textToHide) {
-    // Upewnij się, że wszystkie elementy zostały znalezione przed wykonaniem dalszych operacji
+// Funkcja do przesuwania zawartości sekcji w bok
+function moveSectionContent(section, scrollPosition) {
+  if (!section) {
     return;
   }
 
-  const heroBottom = hero.getBoundingClientRect().bottom;
-  const aboutTop = about.getBoundingClientRect().top;
+  const sectionTop = section.getBoundingClientRect().top;
+  const sectionHeight = section.clientHeight;
+  const scrollOffset = scrollPosition - sectionTop;
 
-  if (heroBottom > aboutTop) {
-    console.log("Tekst zawija się na sekcję 'About'.");
-    // Ukrywamy element z tekstem
-    textToHide.style.display = 'none';
-  }
+  // Ustalamy, jak daleko przesunąć zawartość sekcji w bok
+  const translateX = scrollOffset * 0.5;
+
+  // Ograniczamy przesunięcie, aby tekst nie znikał za sekcją
+  const maxTranslateX = section.clientWidth;
+  const translatedX = Math.min(translateX, maxTranslateX);
+
+  section.style.transform = `translateX(${translatedX}px)`;
 }
 
-
-
-// Efekt paralaksy w hero sekcji
-function parallaxEffect() {
-  const parallax = document.querySelector('#hero');
-  let scrollPosition = window.pageYOffset;
-
-  parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
+// Funkcja do przesuwania zawartości wszystkich sekcji w bok
+function moveAllSectionContent(scrollPosition) {
+  moveSectionContent(aboutSection, scrollPosition);
+  moveSectionContent(tipsSection, scrollPosition);
+  moveSectionContent(projectsSection, scrollPosition);
+  moveSectionContent(contactSection, scrollPosition);
 }
 
-// Wywołujemy funkcje sprawdzającą przy załadowaniu strony i przy zmianie rozmiaru okna.
-window.addEventListener('load', checkTextWrap);
-window.addEventListener('resize', checkTextWrap);
+// Wywołujemy funkcje przy załadowaniu strony i przy zmianie rozmiaru okna.
+window.addEventListener('load', () => {
+  moveAllSectionContent(window.pageYOffset);
+});
 
-// Dodajemy nasłuchiwanie na scroll, aby aktywować efekt paralaksy
-window.addEventListener('scroll', parallaxEffect);
+window.addEventListener('resize', () => {
+  moveAllSectionContent(window.pageYOffset);
+});
 
-// Animacja nawigacji po kliknięciu
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    // Wyłączamy efekt paralaksy podczas animacji nawigacji
-    window.removeEventListener('scroll', parallaxEffect);
-
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    window.scrollTo({
-      top: targetElement.offsetTop - 50, // Uwzględnienie wysokości nagłówka
-      behavior: 'smooth'
-    });
-
-    // Po zakończeniu animacji, przywracamy efekt paralaksy
-    setTimeout(function () {
-      window.addEventListener('scroll', parallaxEffect);
-    }, 1000); // Dostosuj czas do długości animacji
-  });
+// Dodajemy nasłuchiwanie na scroll, aby aktywować efekt przesuwania zawartości sekcji
+window.addEventListener('scroll', () => {
+  moveAllSectionContent(window.pageYOffset);
 });
